651665d7f302758928f1452d829321469f8b4eba
maminseo <162396068+maminseo@users.noreply.github.com>
2024-04-21 23:33:01 +0900

    ㅁㅁ

---
+++ cpp/4월17수업 실습문제2 .cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Circle {
+	int radius; 
+public:
+	void setRadius(int radius);
+	double getArea();
+}; 
+
+void Circle::setRadius(int radius) {
+    this->radius = radius;
+}
+double Circle::getArea() { return 3.14*radius*radius; }
+
+int main() {
+	cout << "원의 개수";
+	int n, radius;
+	cin >> n; 
+
+	Circle *pArray = new Circle [n];
+	for(int i=0; i<n; i++) {
+		cout << "원 " << i+1 << "의 반지름:"; 
+		cin >> radius;
+		pArray[i].setRadius(radius); 
+	}
+
+	int count =0; 
+	Circle* p = pArray;
+	for(int i=0; i<n; i++) {
+		if(p->getArea() >= 100) 
+			count++;
+		p++;
+	}
+	cout << endl << "면적이 100보다 큰 원은" << count << "개 입니다" << endl;
+
+	delete [] pArray; 
+}
