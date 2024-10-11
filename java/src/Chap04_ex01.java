3c8f6fde88dfec74a68ef6f68785b99004242d77
maminseo <162396068+maminseo@users.noreply.github.com>
2024-10-11 10:40:47 +0900

    upload

---
+++ java/src/Chap04_ex01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Chap04_ex01 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n1 = s.nextInt()
+        int n2 = s.nextInt()
+
+        System.out.println("n1 : " + n1);
+        System.out.println("n2 : " + n2);
+    }
+}
