7b739a07d86fa1ca5edd3f9dafdbb1e3917e7ab8
maminseo <162396068+maminseo@users.noreply.github.com>
2024-12-04 14:18:36 +0900

    up

---
+++ .history/cpp/뽑기.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+class 뽑기기계 {
+  string name;
+  cin >> name;
+
+}
---
+++ java/src/javatest.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class javatest {
+
+  class book{
+    
+
+  }
+
+  public static void main(String[] args) {
+    Scanner s =new Scanner(System.in);
+    System.out.println("첫번째 숫자를 입력하세요");
+    int n1 = s.nextInt();
+    s.nextLine();
+    System.out.println("두번째 숫자를 입력하세요");
+    int n2 = s.nextInt();
+    s.nextLine();
+    System.out.println("세번째 숫자를 입력하세요");
+    int n3 = s.nextInt();
+    s.nextLine();
+  }
+  
+}
